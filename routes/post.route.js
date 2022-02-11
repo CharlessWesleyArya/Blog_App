56380758c329dc0ae5acde481c9742967c254936
Charless Wesley Arya <wesleyarya40@gmail.com>
2022-02-11 20:24:01 +0530

    created post

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,16 @@
 var router = require('express').Router();
 const authMiddleware = require('../middlewares/auth.middleware')
-router.get('/', authMiddleware, (req, res) => {
+router.use(authMiddleware)
+router.get('/',(req, res) => {
+    res.render('pages/post',{
+        username:req.session?req.session.username:null,
+        posts:[{
+            "title":"this is title",
+            "description":"this is description"
+        }]
+    })
+})
+router.post('/',(req,res)=>{
     res.render('pages/post')
 })
 
--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,52 +1,49 @@
 const router = require('express').Router()
 
 const UserModel = require('../models/user.model')
+const authMiddleware=require('../middlewares/auth.middleware')
 
-router.get('/', (req, res) => {
-    res.render('pages/home',{
-        username:req.session.username
+router.get('/',authMiddleware, (req, res) => {
+    res.render('pages/home', {
+        username: req.session ? req.session.username : null
     })
 })
 
 router.get('/about', (req, res) => {
-    res.render('pages/about',{
-        username:req.session.username
+    res.render('pages/about', {
+        username: req.session ? req.session.username : null
     })
 })
 router.get('/signup', (req, res) => {
-    res.render('pages/signup',{
-        username:req.session.username
+    res.render('pages/signup', {
+        username: req.session ? req.session.username : null
     })
 })
 router.get('/signin', (req, res) => {
-    res.render('pages/signin',{
-        username:req.session.username
+    res.render('pages/signin', {
+        username: req.session ? req.session.username : null
     })
 })
 router.post('/signin', async (req, res) => {
     var { username, password } = req.body
     var user = await UserModel.findByUserName(username.toLowerCase());
     if (user && user.password === password) {
-        req.session.username=username;
-        return res.redirect('/',{
-            username:req.session.username
-        })
+        req.session.username = username;
+        return res.redirect('/')
     }
 })
 router.post('/signup', async (req, res) => {
     const { username, name, mobile, password } = req.body;
     var isPresent = await UserModel.checkUserName(username);
     if (isPresent) {
-        return ;
+        return;
     }
     await UserModel.create({ ...req.body, username: username.toLowerCase() })
     req.session.username = username;
-    res.redirect('/',{
-        username:req.session.username
-    })
+    res.redirect('/')
 })
 
-router.get('/logout',async (req,res)=>{
+router.get('/logout', async (req, res) => {
     req.session.destroy();
     return res.redirect('/signin')
 })
